a6caa5f195c7d7fa95adde41d2f44ac133856db3
Ranford <ranfordsaisamy@live.com>
2024-03-18 21:54:03 +0000

    Amend type annotation on array func

--- utils.lua
+++ utils.lua
@@ -3,7 +3,8 @@ local Utils = {}
 ---@generic A
 ---@generic B
 ---@param array A[]
----@param func function(elem: A, ...):B
+---@param func function(elem: A, ...: any):B
+---@param ... any
 ---@return B[]
 function Utils.array_func (array, func, ...)
     local res = {}
